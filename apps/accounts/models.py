66017fa1a9fe2aba944c9c6cbc3b14e319e311bd
Elianamos <elianamos29@gmail.com>
2025-01-09 09:17:35 +0300

    implement a custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,2 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = [
+        ("admin", "admin"), 
+        ("pharmacy", "pharmacy"),
+        ("user", "user"),
+    ]
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
