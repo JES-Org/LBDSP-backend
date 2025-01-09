322a251476fcec6e0c71159b4af9eebe14ba18f2
Elianamos <elianamos29@gmail.com>
2025-01-09 09:21:09 +0300

    add apps and configure restframework

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'apps.accounts.CustomUser'
+
 
 # Application definition
 
@@ -25,6 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third party
+    'rest_framework',
+
+    #local
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +94,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 
 # Internationalization
