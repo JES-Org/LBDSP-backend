4664e2d13a433a2fe5f51312298192dde805ce24
Elianamos <elianamos29@gmail.com>
2025-01-12 18:42:34 +0300

    add pharmacy detail apiview

--- apps/pharmacy/views/pharmacy.py
+++ apps/pharmacy/views/pharmacy.py
@@ -20,3 +20,40 @@ class PharmacyAPIView(APIView):
             
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class PharmacyDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Pharmacy.objects.get(pk=pk)
+        except Pharmacy.DoesNotExist:
+            return None
+    
+    def get(self, request, pk, *args, **kwargs):
+        pharmacy = self.get_object(pk)
+        if not pharmacy:
+            return Response({"error": "Pharmacy not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = PharmacySerializer(pharmacy)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk, *args, **kwargs):
+        pharmacy = self.get_object(pk=pk)
+        if not pharmacy:
+            return Response({"error": "Pharmacy not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = PharmacySerializer(pharmacy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, *args, **kwargs):
+        pharmacy = self.get_object(pk)
+        if not pharmacy:
+            return Response({"error": "Pharmacy not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        pharmacy.delete()
+
+        return Response({"message": "Pharmacy deleted"}, status=status.HTTP_204_NO_CONTENT)
