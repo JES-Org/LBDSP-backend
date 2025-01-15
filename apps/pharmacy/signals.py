eb9f6a74a17266572f8a387971521c3f2aebd3ed
Elianamos <elianamos29@gmail.com>
2025-01-15 18:24:27 +0300

    add signal for pharmacist profile creation

---
+++ apps/pharmacy/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from apps.accounts.models import CustomUser
+from apps.pharmacy.models.pharmacist import Pharmacist
+
+@receiver(post_save, sender=CustomUser)
+def create_pharmacist_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "pharmacist":
+        Pharmacist.objects.create(user=instance)
+
+@receiver(post_save, sender=CustomUser)
+def save_pharmacist_profile(sender, instance, **kwargs):
+    if instance.role == "pharmacist" and hasattr(instance, "pharmacist_profile"):
+        instance.pharmacist_profile.save()
