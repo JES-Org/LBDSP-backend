a529d67d6205644d8c91026ea84cb637bc06dbb5
Elianamos <elianamos29@gmail.com>
2025-01-16 14:03:31 +0300

    add customUserApiview

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -10,10 +10,35 @@ from .serializers import RegistrationSerializer
 
 User = get_user_model()
 
-# class CustomUserViewset(viewsets.ModelViewSet):
-#     permission_classes = [AllowAny]
-#     queryset = User.objects.all()
-#     serializer_class = CustomUserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+
+class CustomUserAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None, *args, **kwargs):
+        try:
+            user = CustomUser.objects.get(pk=pk)
+            user.delete()
+            return Response({"detail": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+        except CustomUser.DoesNotExist:
+            return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
 
 class RegistrationAPIView(APIView):
     permission_classes = [AllowAny]
