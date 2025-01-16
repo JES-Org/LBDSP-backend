a5f3c1a4e2bb6820889d951e4e0b5db09e02daa1
Elianamos <elianamos29@gmail.com>
2025-01-16 15:41:32 +0300

    add pharmacist views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,14 +3,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from django.urls import path, include
 
-from .views import RegistrationAPIView
-
-# router = DefaultRouter()
-# router.register('users', CustomUserViewset)
+from .views import RegistrationAPIView, CustomUserAPIView
 
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('', include(router.urls)),
+    path('users/', CustomUserAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', CustomUserAPIView.as_view(), name='user-detail-delete'),
 ]
--- apps/pharmacy/admin.py
+++ apps/pharmacy/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models.pharmacy import Pharmacy
 from .models.medication import Medication, Category
+from .models.pharmacist import Pharmacist
 
 admin.site.register(Pharmacy)
 admin.site.register(Medication)
 admin.site.register(Category)
+admin.site.register(Pharmacist)
