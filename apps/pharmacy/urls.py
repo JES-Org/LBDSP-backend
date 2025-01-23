913d1b7465b0c209b77034e7366f6edda03cb275
Elianamos <elianamos29@gmail.com>
2025-01-23 09:36:42 +0300

    add pharmacy medication search apiview

--- apps/pharmacy/urls.py
+++ apps/pharmacy/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views.pharmacy import PharmacyAPIView, PharmacyDetailAPIView, PharmacySearchAPIView, NearbyPharmacyAPIView, PharmacyRegistrationView
-from .views.medication import CategoryAPIView, MedicationDetailAPIView, MedicationAPIView, PharmacyMedicationsAPIView, PharmacyMedicationDetailAPIView, MedicationSearchAPIView
+from .views.medication import CategoryAPIView, MedicationDetailAPIView, MedicationAPIView, PharmacyMedicationsAPIView, PharmacyMedicationDetailAPIView, MedicationSearchAPIView, PharmacyMedicationSearchAPIView
 
 urlpatterns = [
     path('pharmacies/', PharmacyAPIView.as_view(), name='pharmacy-list-create'),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('pharmacies/<int:pharmacy_id>/medications/', PharmacyMedicationsAPIView.as_view(), name='pharmacy-medication-list-create'),
     path('pharmacies/<int:pharmacy_id>/medications/<int:medication_id>/', PharmacyMedicationDetailAPIView.as_view(), name='pharmacy-medication-detail'),
     path('pharmacies/nearby/', NearbyPharmacyAPIView.as_view(), name='nearby-pharmacies'),
+    path('pahrmacies/<int:pharmacy_id>/medications/search/', PharmacyMedicationSearchAPIView.as_view(), name='pharmacy-medication-search'),
     
     path('medications/', MedicationAPIView.as_view(), name='medication-list-create'),
     path('medications/<int:pk>/', MedicationDetailAPIView.as_view(), name='medication-detail'),
