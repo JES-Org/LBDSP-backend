520bba622143003e4427837c75c639ab91ccde3e
Elianamos <elianamos29@gmail.com>
2025-02-05 14:51:18 +0300

    add drf-yasg for api documentation

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,24 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.contrib import admin
 from django.urls import path, include
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FetanFews API",
+        default_version='v1',
+        description="An API for a Location-Based Drug Search Platform.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('apps.accounts.urls')),
-    path('api/', include('apps.pharmacy.urls'))
+    path('api/', include('apps.pharmacy.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
