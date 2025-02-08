2932b2a0eec4eaf567857407dda0626dd4cd9f57
jenberu <jemberu0970@gmail.com>
2025-02-08 19:31:35 +0300

    Enhance PharmacistSerializer to include nested user and pharmacy details with write-only fields for IDs

--- apps/pharmacy/serializers/pharmacist.py
+++ apps/pharmacy/serializers/pharmacist.py
@@ -2,14 +2,22 @@ from rest_framework import serializers
 
 from apps.pharmacy.models.pharmacist import Pharmacist
 from apps.accounts.serializers import CustomUserSerializer
+from .pharmacy import PharmacySerializer
+from apps.pharmacy.models.pharmacy import Pharmacy
+from apps.accounts.models import CustomUser
 
 
 class PharmacistSerializer(serializers.ModelSerializer):
-    pharmacy_name = serializers.ReadOnlyField(source='pharmacy.name')
-    user_name = serializers.ReadOnlyField(source='user.first_name')
+    user = CustomUserSerializer(read_only=True)  
+    pharmacy = PharmacySerializer(read_only=True)  
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.all(), source="user", write_only=True
+    ) 
+    pharmacy_id = serializers.PrimaryKeyRelatedField(
+        queryset=Pharmacy.objects.all(), source="pharmacy", write_only=True
+    )  
 
     class Meta:
         model = Pharmacist
         fields='__all__'
         read_only_fields = ['created_at']
-        extra_fields = ['pharmacy_name',"user_name"]
