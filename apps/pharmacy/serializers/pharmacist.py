d26f43f0a855a32238fb99be2a66c2d874cb52d4
jenberu <jemberu0970@gmail.com>
2025-02-11 09:23:21 +0300

    Add PharmacistUpdateSerializer to handle updates for pharmacist and pharmacy details

--- apps/pharmacy/serializers/pharmacist.py
+++ apps/pharmacy/serializers/pharmacist.py
@@ -21,3 +21,28 @@ class PharmacistSerializer(serializers.ModelSerializer):
         model = Pharmacist
         fields='__all__'
         read_only_fields = ['created_at']
+
+class PharmacistUpdateSerializer(serializers.ModelSerializer):
+    pharmacy = PharmacySerializer()  
+
+    class Meta:
+        model = Pharmacist
+        fields = ['license_number', 'license_image', 'pharmacy']  # Allow updates to these fields
+
+    def update(self, instance, validated_data):
+        # Update Pharmacist details
+        pharmacy_data = validated_data.pop('pharmacy', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        # Update pharmacy details if provided
+        if pharmacy_data and instance.pharmacy:
+            pharmacy_instance = instance.pharmacy
+            for attr, value in pharmacy_data.items():
+                setattr(pharmacy_instance, attr, value)
+            pharmacy_instance.save()
+
+        return instance
