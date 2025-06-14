7d60b37a229d6aaa7004c56e9667d7e85c2e1e15
jenberu <jemberu0970@gmail.com>
2025-06-14 07:56:33 +0300

    Update media URL serving condition to include production environment

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('api/', include('apps.pharmacy.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
-if settings.DEBUG or settings.ENVIRONMENT == 'production':
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
