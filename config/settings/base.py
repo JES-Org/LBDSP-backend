4afbb4ca8dbeeaf17db0702db5bcd66441afffe4
jenberu <jemberu0970@gmail.com>
2025-06-14 12:27:30 +0300

    Fix CORS settings by updating frontend URL format

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,14 +173,13 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    'https://lbdsp-frontend.vercel.app',
+    "https://lbdspfrontend.vercel.app",
     "https://fetanfews.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
-     'https://lbdsp-frontend.vercel.app',
-
+    "https://lbdspfrontend.vercel.app",
     "https://fetanfews.up.railway.app",
 ]
 
